7faff761c6ddb4914d1d9152af1160afa7cdb150
zhoujun134 <1161386101@qq.com>
2025-07-27 18:04:43 +0800

    init

--- mcp-clent/src/test/java/com/zj/ai/mcp/clent/McpClientApplicationTests.java
+++ mcp-clent/src/test/java/com/zj/ai/mcp/clent/McpClientApplicationTests.java
@@ -1,13 +1,17 @@
 package com.zj.ai.mcp.clent;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class McpClentApplicationTests {
+@Slf4j
+class McpClientApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("测试代码");
+
     }
 
 }
